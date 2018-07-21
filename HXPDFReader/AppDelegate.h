d5cf2d05b5b1a07efa824b58a315e3c8d5c5116e
TheLittleBoy <wangxa@edu-edu.com.cn>
2018-07-21 13:35:54 +0800

    Initial Commit

---
+++ HXPDFReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXPDFReader
+//
+//  Created by Mac on 2018/7/21.
+//  Copyright © 2018年 华夏大地教育网. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXPDFReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXPDFReader
+//
+//  Created by Mac on 2018/7/21.
+//  Copyright © 2018年 华夏大地教育网. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
